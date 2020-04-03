cd631cb5bc57cbac69c28b675ebdb97f14902844
virgoone <w2008second@gmail.com>
2020-04-03 11:44:17 +0800

    feat: 修改提示文字和安装依赖event

--- src/utils/dependencies.ts
+++ src/utils/dependencies.ts
@@ -1,57 +1,60 @@
-import chalk from 'chalk'
-import execa from 'execa'
-import readline from 'readline'
+import chalk from "chalk";
+import execa from "execa";
+import readline from "readline";
 
 export const installDependencies = (targetPath: string) => {
   return new Promise((resolve, reject) => {
-    const child = execa('yarn', [], {
+    const child = execa("yarn", [], {
       cwd: targetPath,
-      stdio: ['inherit', 'inherit', 'pipe'],
-    })
+      stdio: ["inherit", "inherit", "pipe"]
+    });
 
-    child.stderr!.on('data', buf => {
-      const str = buf.toString()
+    child.stderr!.on("data", buf => {
+      const str = buf.toString();
       if (/warning/.test(str)) {
-        return
+        return;
       }
       // progress bar
-      const progressBarMatch = str.match(/\[.*\] (\d+)\/(\d+)/)
+      const progressBarMatch = str.match(/\[.*\] (\d+)\/(\d+)/);
       if (progressBarMatch) {
         // since yarn is in a child process, it's unable to get the width of
         // the terminal. reimplement the progress bar ourselves!
-        renderProgressBar(progressBarMatch[1], progressBarMatch[2])
-        return
+        renderProgressBar(progressBarMatch[1], progressBarMatch[2]);
+        return;
       }
-      process.stderr.write(buf)
-    })
+      process.stderr.write(buf);
+    });
 
-    child.on('exit', code => {
+    child.on("error", err => {
+      reject(err);
+    });
+
+    child.on("close", code => {
       if (code !== 0) {
-        reject(new Error('command failed: yarn'))
-        return
+        reject(new Error("command failed: yarn"));
+        return;
       }
-      resolve()
-    })
-  })
-}
+      resolve();
+    });
+  });
+};
 
 function renderProgressBar(curr: number, total: number) {
-  const ratio = Math.min(Math.max(curr / total, 0), 1)
-  const bar = ` ${curr}/${total}`
-  const availableSpace = Math.max(0, process.stderr.columns - bar.length - 3)
-  const width = Math.min(total, availableSpace)
-  const completeLength = Math.round(width * ratio)
-  const complete = `#`.repeat(completeLength)
-  const incomplete = `-`.repeat(width - completeLength)
-  toStartOfLine(process.stderr)
-  process.stderr.write(`[${complete}${incomplete}]${bar}`)
+  const ratio = Math.min(Math.max(curr / total, 0), 1);
+  const bar = ` ${curr}/${total}`;
+  const availableSpace = Math.max(0, process.stderr.columns - bar.length - 3);
+  const width = Math.min(total, availableSpace);
+  const completeLength = Math.round(width * ratio);
+  const complete = `#`.repeat(completeLength);
+  const incomplete = `-`.repeat(width - completeLength);
+  toStartOfLine(process.stderr);
+  process.stderr.write(`[${complete}${incomplete}]${bar}`);
 }
 
 function toStartOfLine(stream: NodeJS.WriteStream) {
   if (!chalk.supportsColor) {
-    stream.write('\r')
-    return
+    stream.write("\r");
+    return;
   }
-  readline.cursorTo(stream, 0)
+  readline.cursorTo(stream, 0);
 }
-
