629166fb96f6d8065fc2733475766144a86624c0
virgoone <w2008second@gmail.com>
2020-04-03 13:20:21 +0800

    fix: add log

--- src/utils/dependencies.ts
+++ src/utils/dependencies.ts
@@ -26,10 +26,21 @@ export const installDependencies = (targetPath: string) => {
     });
 
     child.on("error", err => {
+      console.log('error call -->', err)
       reject(err);
     });
 
+    child.on("exit", code => {
+      console.log('exit call -->', code)
+      if (code !== 0) {
+        reject(new Error("command failed: yarn"));
+        return;
+      }
+      resolve();
+    });
+
     child.on("close", code => {
+      console.log('close call -->', code)
       if (code !== 0) {
         reject(new Error("command failed: yarn"));
         return;
