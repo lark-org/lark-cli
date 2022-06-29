5441b16b7dc28cc4dbf32bd558ee451681a984c4
virgoone <w2008second@gmail.com>
2022-06-29 15:51:23 +0800

    feat: 增加包管理器和修改创建项目命令

---
+++ src/utils/runners/abstract.runner.ts
@@ -0,0 +1,55 @@
+/* eslint-disable no-console */
+import * as chalk from 'chalk'
+import { ChildProcess, spawn, SpawnOptions } from 'child_process'
+
+const RUNNER_EXECUTION_ERROR = (command: string) =>
+  `\nFailed to execute command: ${command}`
+
+export class AbstractRunner {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(protected binary: string, protected args: string[] = []) {}
+
+  public async run(
+    command: string,
+    collect = false,
+    cwd: string = process.cwd()
+  ): Promise<null | string> {
+    const args: string[] = [command]
+    const options: SpawnOptions = {
+      cwd,
+      stdio: collect ? 'pipe' : 'inherit',
+      shell: true
+    }
+    return new Promise<null | string>((resolve, reject) => {
+      const child: ChildProcess = spawn(
+        `${this.binary}`,
+        [...this.args, ...args],
+        options
+      )
+      if (collect) {
+        child.stdout!.on('data', (data) =>
+          resolve(data.toString().replace(/\r\n|\n/, ''))
+        )
+      }
+      child.on('close', (code) => {
+        if (code === 0) {
+          resolve(null)
+        } else {
+          console.error(
+            chalk.red(RUNNER_EXECUTION_ERROR(`${this.binary} ${command}`))
+          )
+          reject()
+        }
+      })
+    })
+  }
+
+  /**
+   * @param command
+   * @returns The entire command that will be ran when calling `run(command)`.
+   */
+  public rawFullCommand(command: string): string {
+    const commandArgs: string[] = [...this.args, command]
+    return `${this.binary} ${commandArgs.join(' ')}`
+  }
+}
---
+++ src/utils/runners/runner.factory.ts
@@ -0,0 +1,24 @@
+import * as chalk from 'chalk'
+import { Runner } from './runner'
+import { NpmRunner } from './npm.runner'
+import { PnpmRunner } from './pnpm.runner'
+import { YarnRunner } from './yarn.runner'
+
+export class RunnerFactory {
+  // eslint-disable-next-line consistent-return
+  public static create(runner: Runner) {
+    switch (runner) {
+      case Runner.NPM:
+        return new NpmRunner()
+
+      case Runner.YARN:
+        return new YarnRunner()
+
+      case Runner.PNPM:
+        return new PnpmRunner()
+
+      default:
+        console.info(chalk.yellow(`[WARN] Unsupported runner: ${runner}`))
+    }
+  }
+}
