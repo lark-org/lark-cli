50c8b874274300aaf8eec9e5f01ec7d175b74180
virgoone <me@virgo.one>
2022-11-05 22:09:03 +0800

    fix(project): 解决因为构建类型问题导致的build失败问题

--- src/lib/compiler/webpack-plugins/mfsu/index.ts
+++ src/lib/compiler/webpack-plugins/mfsu/index.ts
@@ -4,9 +4,9 @@ import { MFSU } from '@umijs/mfsu'
 import paths from '@/lib/compiler/variables/paths'
 import builds from '@/lib/compiler/variables/builds'
 
-// @ts-ignore
 const mfsu = new MFSU({
   cwd: paths.appPath,
+  // @ts-ignore
   implementor: webpack,
   buildDepWithESBuild: builds.transpiler === 'esbuild'
 })
