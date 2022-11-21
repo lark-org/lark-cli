9aa5cce39b2fa6a3526a154209b3b791752343a0
virgoone <w2008second@gmail.com>
2022-11-21 17:45:48 +0800

    fix: 解决全局命令在react项目外使用时报错问题

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import './utils/alias'
 import { Command } from 'commander'
 import { CommandLoader } from './commands'
 import variables from './lib/compiler/variables'
+import paths from './lib/compiler/variables/paths'
+import { ERROR_PREFIX } from './utils/log'
 
 process.on('unhandledRejection', (err) => {
   console.error('unhandledRejection>', err)
@@ -13,6 +15,7 @@ process.on('uncaughtException', (err) => {
   console.error('uncaughtException>', err)
   throw err
 })
+const requiredVariableKeys = ['APP_NAME', 'VERSION']
 
 const bootstrap = (): void => {
   const program = new Command()
@@ -23,8 +26,16 @@ const bootstrap = (): void => {
       require('../package.json').version,
       '-v, --version'
     )
+    .option('-t, --trace', 'display trace statements for commands')
     .usage('<command> [options]')
     .hook('preAction', (thisCommand, actionCommand) => {
+      if (thisCommand.opts().trace) {
+        console.log(
+          `About to call action handler for subcommand: ${actionCommand.name()}`
+        )
+        console.log('arguments: %O', actionCommand.args)
+        console.log('options: %o', actionCommand.opts())
+      }
       const { appEnv, nodeEnv } = actionCommand.opts() || {}
 
       process.env.NODE_ENV = nodeEnv || 'production'
@@ -35,6 +46,26 @@ const bootstrap = (): void => {
       variables.__DEV__ = process.env.NODE_ENV === 'development'
       variables.APP_ENV = process.env.APP_ENV
       variables.SENTRY_RELEASE = `${variables.APP_ENV}-${variables.RELEASE}`
+
+      if (['start', 'build'].includes(actionCommand.name())) {
+        if (!paths.appIndex || !paths.appHtml) {
+          console.error(`\n${ERROR_PREFIX} 缺少入口文件\n`)
+          process.exit(1)
+        }
+        let checkRequiredKey = true
+        // eslint-disable-next-line no-restricted-syntax
+        for (const requiredKey of requiredVariableKeys) {
+          if (!variables[requiredKey]) {
+            checkRequiredKey = false
+            console.error(
+              `\n${ERROR_PREFIX} please config required variable: ${requiredKey}\n`
+            )
+          }
+        }
+        if (!checkRequiredKey) {
+          process.exit(1)
+        }
+      }
     })
     .helpOption('-h, --help', 'Output usage information.')
 
--- src/lib/compiler/variables/paths.ts
+++ src/lib/compiler/variables/paths.ts
@@ -6,57 +6,46 @@ import {
 } from './utils'
 import { getCustomConfig } from './custom-config'
 
-const appPath = resolveApp('.')
-const appTsConfig = resolveApp('tsconfig.json')
-const appJsConfig = resolveApp('jsconfig.json')
-const appNodeModules = resolveApp('node_modules')
-const appWebpackCache = resolveApp('node_modules/.cache')
-const appSrc = getProjectFilePath('./src')
-const appBuild = getProjectFilePath('./dist')
-const appPublic = getProjectFilePath('./public')
-
-const swSrc = resolveModule(resolveApp, './src/service-worker')
-const appIndex = resolveModule(resolveApp, './src/index')
-const appPackageJson = getProjectFilePath('./package.json')
-const yarnLockFile = getProjectFilePath('./yarn.lock')
-const appPolyfill = require.resolve('../polyfills/index.js')
-const appHtml = getProjectFilePath('./src/index.html')
-
-const customPaths = getCustomConfig().paths
-
-const packageManager = appPackageJson
-  ? require(appPackageJson).packageManager?.split('@')?.[0] || 'npm'
-  : 'npm'
-
-const appHttpsConfig = getWorkspacePath('./.lark')
-
 const appHttpsKey = (host: string) =>
   getWorkspacePath(`./.lark/${host}-key.pem`)
 const appHttpsCert = (host: string) =>
   getWorkspacePath(`./.lark/${host}-cert.pem`)
+const customPaths = getCustomConfig().paths
+const appHttpsConfig = getWorkspacePath('./.lark')
 
-export const paths = {
-  appPath,
-  appIndex,
-  appSrc,
-  appBuild,
-  appPublic,
-  appHtml,
-  appPolyfill,
-  yarnLockFile,
-  swSrc,
-  appPackageJson,
-  appTsConfig,
-  appJsConfig,
-  appWebpackCache,
-  appNodeModules,
-  appPackageManager: packageManager,
-  appHttpsConfig,
+const paths: Record<string, any> = {
   appHttpsKey,
   appHttpsCert,
-  ...customPaths
+  appHttpsConfig
 }
+try {
+  const appPackageJson = getProjectFilePath('./package.json')
+
+  paths.appPackageJson = appPackageJson
+  paths.appPath = resolveApp('.')
+  paths.appTsConfig = resolveApp('tsconfig.json')
+  paths.appJsConfig = resolveApp('jsconfig.json')
+  paths.appNodeModules = resolveApp('node_modules')
+  paths.appWebpackCache = resolveApp('node_modules/.cache')
+  paths.appSrc = getProjectFilePath('./src')
+  paths.appBuild = getProjectFilePath('./dist')
+  paths.appPublic = getProjectFilePath('./public')
+
+  paths.swSrc = resolveModule(resolveApp, './src/service-worker')
+  paths.appIndex = resolveModule(resolveApp, './src/index')
+  paths.yarnLockFile = getProjectFilePath('./yarn.lock')
+  paths.appPolyfill = require.resolve('../polyfills/index.js')
+  paths.appHtml = getProjectFilePath('./src/index.html')
+
+  paths.customPaths = getCustomConfig().paths || {}
+
+  paths.packageManager = appPackageJson
+    ? require(appPackageJson).packageManager?.split('@')?.[0] || 'npm'
+    : 'npm'
+  // eslint-disable-next-line no-empty
+} catch (error) {}
 
 export default {
-  ...paths
+  ...paths,
+  ...customPaths
 }
